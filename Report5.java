26ea02ea77fb9d297fd24d5ac22aa0ac1ba74f64
勇夢亜 <isamimuua@isamuyumeanoMacBook-Air.local>
2023-12-22 14:17:20 +0900

    step3 finished

--- Report5.java
+++ Report5.java
@@ -1,12 +1,6 @@
 public class Report5{
-    public static void main(String[] args) {
-        String str = null;
-        try{
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptio発生");
-            System.out.println(e.getMessage());
-        }
-        
+    public static void main(String[] args){
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }
 }
